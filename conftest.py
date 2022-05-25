de337e705682d16f2c4178ef30e1649cf5947370
kirillqagarant <82926917+kirillqagarant@users.noreply.github.com>
2022-05-25 16:58:57 +0600

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 import pytest
 from time import sleep
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 def set_headless(headless):
@@ -15,7 +16,7 @@ def set_headless(headless):
 def browser():
     sleep(40)
 #     driver = webdriver.Chrome(ChromeDriverManager().install(), options=set_headless(True))
-    driver = webdriver.Remote(command_executor='http://127.0.0.1:9222/wd/hub')
+    driver = webdriver.Remote(command_executor='http://127.0.0.1:9222/wd/hub', desired_capabilities=DesiredCapabilities.CHROME)
     driver.set_window_size(1920, 1080)
     driver.get("https://www.google.ru")
     yield driver.title
